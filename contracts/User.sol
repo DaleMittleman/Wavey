d09a2a66ec0075288ffc6a04b334906faf8847ff
Albert Hermida <alberthermida@gmail.com>
2017-12-04 11:14:14 -0500

    Added utility functions and smashed some bugs related to indexing into friends

--- contracts/User.sol
+++ contracts/User.sol
@@ -1,40 +1,134 @@
 pragma solidity ^0.4.15;
+import "./WNS.sol";
+import "./Owned.sol";
 
-/// @title Self-Sovereign Social Networking
-contract User {
-
+/**
+ * @title Self-Sovereign Social Networking
+ */
+contract User is Owned {
     bytes32 private id;
-    mapping (bytes32 => address) private friends;
-    mapping (bytes32 => address) private received;
-    mapping (address => bool) private _owners;
+    bytes32 private name;
 
-    /// @dev Contract constructor instantiates user with owner
-    /// @param name The WNS name that a user wants to register
-    function User(bytes32 name) public {
-        id = name;
+    event LogDescription(string indexed description);
+    event LogName(string indexed name);
+    event LogImage(string indexed img);
+
+    enum Status { unset, sent, received }
+
+    mapping (bytes32 => User) public friends;
+    mapping (bytes32 => Status) public pending;
+    mapping (address => bool) private _permissioned;
+
+    WNS wns = WNS(0x7502ab3685672f27412cfcc7a6dbe8bef8d9909d);
+
+    /**
+     * @dev Require that caller is permissioned or owner
+     */
+    modifier onlyPermissioned() {
+        require(_permissioned[msg.sender] == true || owner == msg.sender);
+        _;
     }
 
-    /// @dev Simply gets a user's name / WNS id
+    /**
+     * @dev Contract constructor instantiates user with owner
+     * @param _id The WNS name that a user wants to register
+     */
+    function User(bytes32 _id) public {
+        id = _id;
+        wns.insert(this, _id);
+    }
+
+    /**
+     * @dev Simply gets a user's name / WNS id
+     */
     function getName() public constant returns (bytes32) {
+        return name;
+    }
+
+    /**
+     * @dev Simply gets a user's name / WNS id
+     */
+    function getId() public constant returns (bytes32) {
         return id;
     }
 
-    /// @dev Adds a friend to the contract from pending
-    /// @param fid The friend's id that we're adding from pending
-    /// Note: You can be friends with somebody without reciprocation
-    function addFriend(bytes32 fid) public {
-        require(received[fid] != address(0));
-        User u = User(received[fid]);
-        friends[u.getName()] = received[fid];
-        delete received[fid];
+    /**
+     * @dev Set user's name
+     * @param _name The name that a user wants to set
+     */
+    function setName(bytes32 _name) public onlyOwner {
+        name = _name;
     }
 
-    /// @dev Adds a friend to pending list
-    /// @param fid Id of a potential friend's contract
-    /// Note: Hooligans can send empty fid, or call this from a non-User
-    ///  Note: Get contract addr from WNS instead of msg.sender later
-    function request(bytes32 fid) public {
-        require(fid != "");
-        received[fid] = msg.sender;
+    /**
+     * @dev Adds a friend to the contract from pending
+     * @param friend The friend's id that we're adding from pending
+     */
+    function acceptFriend(bytes32 friend) public onlyPermissioned {
+        require(pending[friend] == Status.received);
+        friends[friend] = wns.get(friend);
+    }
+
+    /**
+     * @dev Adds a friend to pending list
+     * @param friend Id of a potential friend's contract
+     * @notice gets foreign user contract from WNS
+     */
+    function addFriend(bytes32 friend) public onlyPermissioned {
+        pending[friend] = Status.sent;
+        require(wns.contains(friend));
+        User u = wns.get(friend);
+        u.addPending();
+    }
+
+    /**
+     * @dev Removes a friend from friend's list
+     * @param friend The id of a friend
+     */
+    function removeFriend(bytes32 friend) public onlyPermissioned {
+        delete friends[friend];
+    }
+
+    /**
+     * @dev Adds to a friend's pending list
+     * @notice Queries the caller's contract
+     */
+    function confirm() public {
+        User u = User(msg.sender);
+        require(pending[u.getId()] == Status.sent);
+        delete pending[u.getId()];
+        friends[u.getId()] = u;
+    }
+
+    /**
+     * @dev Adds to a friend's pending list
+     * @notice Queries the caller's contract
+     */
+     function addPending() public {
+        User u = User(msg.sender);
+        pending[u.getId()] = Status.received;
+     }
+
+    /**
+     * @dev Changes WNS to another address for private networks
+     */
+    function changeWNS(address addr) public onlyOwner {
+        wns = WNS(addr);
+    }
+
+    /**
+     * @dev Adds a permissioned address
+     * @param addr Address that we want to permission
+     */
+    function addPermissioned(address addr) public onlyOwner {
+        _permissioned[addr] = true;
+    }
+
+    /**
+     * @dev Removes a permissioned address
+     * @param addr Address that we want to remove as a permission
+     */
+    function removePermissioned(address addr) public onlyOwner {
+        delete _permissioned[addr];
     }
 }
